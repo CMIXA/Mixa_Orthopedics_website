2b1f779b51c1f03117fd65f4a553fe654d931615
Chris Mixa <91347624+CMIXA@users.noreply.github.com>
2022-07-30 00:58:36 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import './navbar.css';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h1>App</h1>
+      <Footer/>
     </div>
   );
 }
