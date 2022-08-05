95a393c368760fa40244dc53785faa2f0753c405
Chris Mixa <91347624+CMIXA@users.noreply.github.com>
2022-08-05 00:22:46 -0400

    Add files via upload

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,18 +5,32 @@ function Navbar() {
         <div className='navbar'>
             <div className='navbar-wrapper'>
                 <div className='navbar-title-wrapper'>
-                    <div className='navbar-title-image'></div>
+                    <div className='navbar-title-image'>
+                        <img src='bones.png'></img>
+                    </div>
                     <div className='navbar-title'>
                         <h1>Mixa Orthopedics</h1>
                     </div>
                 </div>
                 <div className='navbar-items'>
-                    <div className='navbar-item'>Home</div>
-                    <div className='navbar-item'>Our Services</div>
-                    <div className='navbar-item'>About Us</div>
-                    <div className='navbar-item'>Patient Forms</div>
-                    <div className='navbar-item'>FAQs</div>
-                    <div className='navbar-item'>Contact</div>
+                    <div className='navbar-item'>
+                        <a href='#'>Home</a>
+                    </div>
+                    <div className='navbar-item'>
+                        <a href='#'>Our Services</a>
+                    </div>
+                    <div className='navbar-item'>
+                        <a href='#'>About Us</a>
+                    </div>
+                    <div className='navbar-item'>
+                        <a href='#'>Patient Forms</a>
+                    </div>
+                    <div className='navbar-item'>
+                        <a href='#'>FAQs</a>
+                    </div>
+                    <div className='navbar-item'>
+                        <a href='#'>Contact</a>
+                    </div>
                 </div>
             </div>
         </div>
