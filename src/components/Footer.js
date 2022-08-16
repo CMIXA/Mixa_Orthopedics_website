2e9f007fd267e1ec1894e1dc49738f373427af3a
Chris Mixa <91347624+CMIXA@users.noreply.github.com>
2022-08-16 19:04:46 -0400

    Add files via upload

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
                     <div className='column-1'>
                         <div className='footer-title'>
                             <div className='logo'>
-                                <img></img>
+                                <img src='bones.png'></img>
                             </div>
                             <div className='title'>
                                 <h1>Mixa Orthopedics</h1>
@@ -17,16 +17,50 @@ function Footer() {
                         <div className='footer-button'>
                             <button>Schedule an Appointment</button>
                         </div>
-                        <div>
-                            <p>* Our phone number is available 24/7 for urgent calls</p>
+                        <div className='number'>
+                            <p>*Our phone number is available 24/7 for urgent calls</p>
                         </div>
                     </div>
                     <div className='column-2'>
-                        <div></div>
-                        <div></div>
+                        <div className='column-2-contact'>
+                            <h2>Contact</h2>
+                            <div className='contact-content'>
+                                <p>Phone: 727-321-9644</p>
+                                <p>Fax: 727-321-8580</p>
+                                <p>Billing: 727-955-2295</p>
+                                <p>Email: Email@drmixa.com</p>
+                            </div>
+                        </div>
+                        <div className='column-2-address'>
+                            <h2>Address</h2>
+                            <div className='address-content'>
+                                <p>Mixa Orthopedics</p>
+                                <p>1609 Pasadena Ave. S. Suite 1A South Pasedena, FL 33707</p>
+                            </div>
+                        </div>
                     </div>
                     <div className='column-3'>
-                        <div></div>
+                        <div className='column-3-office'>
+                            <h2>Office Hours</h2>
+                            <div className='days-times'>
+                                <div className='days'>
+                                    <p>Monday</p>
+                                    <p>Tuesday</p>
+                                    <p>Wednesday</p>
+                                    <p>Thursday</p>
+                                    <p>Friday</p>
+                                    <p>Sat/Sun</p>
+                                </div>
+                                <div className='times'>
+                                    <p>9am - 5pm</p>
+                                    <p>9am - 5pm</p>
+                                    <p>9am - 5pm</p>
+                                    <p>9am - 5pm</p>
+                                    <p>9am - 5pm</p>
+                                    <p></p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
