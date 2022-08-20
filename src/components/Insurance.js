e1e4ca36273722388e705cdadc2fb581ce53d313
Chris Mixa <91347624+CMIXA@users.noreply.github.com>
2022-08-20 13:14:56 -0400

    Add files via upload

--- src/components/Insurance.js
+++ src/components/Insurance.js
@@ -7,8 +7,83 @@ const Insurance = () => {
             <div className='insurance-title-container'>
                 <h1>Insurance</h1>
             </div>
-            <div className='insurance-wrapper'>
-                <div className='insurance-content-container'>
+            <div className='insurance-wrappe'>
+                <div className='payment-container'>
+                    <p>Mixa Orthopedics is dedicated to providing you with the highest quality, cost effective medical care. We accept all major credit cards and offer payment installments.</p>
+                </div>
+                <div className='our-insurances-container'>
+                    <h2>Insurance</h2>
+                    <p>We accept nearly all major insurance plans including</p>
+                    <div>
+                        <div>
+                            <ul>
+                                <li>AARP Suplemental</li>
+                                <li>Aetna Elect Choice</li>
+                                <li>Aetna HMO</li>
+                                <li>Aetna Medicare HMO</li>
+                                <li>Aetna PPO</li>
+                                <li>Aetna POS</li>
+                                <li>Aetna Open Access</li>
+                                <li>Aetna Select</li>
+                                <li>Alignment Healthcare</li>
+                                <li>BC Federal</li>
+                                <li>BCBS Highmark</li>
+                                <li>BCBS Medicare Adv. HMO</li>
+                                <li>BCBS Medicare PPO</li>
+                                <li>BCBS PPO</li>
+                                <li>Better Health</li>
+                                <li>Careplus</li>
+                                <li>Champ VA</li>
+                                <li>Cigna Disney</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <ul>
+                                <li>Cigna Medicare HMO</li>
+                                <li>Cigna Medicare PPO</li>
+                                <li>Cigna PPO</li>
+                                <li>Cigna POS</li>
+                                <li>Cigna Open Access</li>
+                                <li>Cigna Indemnity </li>
+                                <li>Coventry</li>
+                                <li>Freedom Health</li>
+                                <li>Freedom Life</li>
+                                <li>Golden Rule</li>
+                                <li>Humana Gold Plus HMO</li>
+                                <li>Humana Medicare HMO</li>
+                                <li>Humana Medicare PPO</li>
+                                <li>Medicare of Florida</li>
+                                <li>Molina Medicare</li>
+                                <li>Optimum</li>
+                                <li>PCHS Multiplan</li>
+                                <li>Preferred Care Partners</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <ul>
+                                <li>Tricare Prime</li>
+                                <li>Tricare Standard</li>
+                                <li>Tricare For Life</li>
+                                <li>UHC AARP Medicare Complete HMO</li>
+                                <li>UHC AARP Medicare Complete PPO</li>
+                                <li>UHC Charter</li>
+                                <li>UHC Choice</li>
+                                <li>UHC Community Dual HMO</li>
+                                <li>UHC Community Dual PPO</li>
+                                <li>UHC Community M*Plus</li>
+                                <li>UHC Compass</li>
+                                <li>UHC Dual Complete HMO</li>
+                                <li>UHC Dual Complete PPO</li>
+                                <li>UHC Navigate</li>
+                                <li>UMR</li>
+                                <li>WellCare</li>
+                                <li>Wellmed- AARP Medicare Complete</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className='not-listed'>
+                    <p>If your insurance plan is not listed above,Please call us at 727-321-9644 to see if your insurance is still eligible.</p>
                 </div>
             </div>
         </main>
