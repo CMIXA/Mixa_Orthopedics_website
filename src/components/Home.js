476c2cc5a7d2af7269c59c1a4a19e4c9fb3cdc43
Chris Mixa <91347624+CMIXA@users.noreply.github.com>
2022-08-22 01:00:42 -0400

    Add files via upload

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import palms from "./images/palms.jpeg"
+import xray from "./images/xray.jpeg"
 
 
 const Home = () => {
@@ -15,7 +17,7 @@ const Home = () => {
                 <div className='our-practice-container'>
                     <div className='wrapper'>
                         <div className='office-building'>
-                            <img alt='office'></img>
+                            <img src={palms} alt='office'></img>
                         </div>
                         <div className=''>
                             <div>
@@ -28,36 +30,49 @@ const Home = () => {
                 </div>
                 <div className='seperator'></div>
                 <div className='our-specialties-container'>
-                    <div className='wrapper'>
-                        <div className=''>
-                            <div>
-                                <h2></h2>
-                                <ul></ul>
-                                <button>Other conditions and treatments</button>
-                            </div>
+                    <div className=''>
+                        <div>
+                            <h2>Our Specialties</h2>
+                            <ul>
+                                <li>Total Joints</li>
+                                <li>Trauma & Fracture Care</li>
+                                <li>Advanced Hip Fracture Care</li>
+                                <li>Sports Medicine</li>
+                                <li>Cortisone & Hyaluronic Acid Injections</li>
+                                <li>Surgery</li>
+                                <li>Conservative Back & Neck Treatment</li>
+                                <li>Hand Conditions</li>
+                                <li>Joint Pain</li>
+                                <li>X-rays, Casting, Splinting</li>
+                            </ul>
+                            <button>Other conditions and treatments</button>
                         </div>
-                        <div className=''>
-                            <img alt='xray'></img>
-                        </div>
+                    </div>
+                    <div className='our-specialties-image'>
+                        <img src={xray} alt='xray'></img>
                     </div>
                 </div>
                 <div className='seperator'></div>
                 <div className='questions-container'>
-                    <div className='wrapper'>
+                    <div>
                         <div>
                             <h1>How can we help?</h1>
                         </div>
-                        <div>
-                            <div>
+                        <div className='questions-wrapper'>
+                            <div className='questions'>
                                 <button>What insurances do we accept?</button>
                                 <button>New patient or need to update your patient forms?</button>
                                 <button>What time should I arrive before my appointment?</button>
                             </div>
-                            <div>
+                            <div className='questions'>
                                 <button>Need a cortisone injection?</button>
                                 <button>Do we offer prescription refills?</button>
                                 <button>Do we offer same day appointments?</button>
                             </div>
+                        </div>
+                        <div>
+                            <h2>Have more questions?</h2>
+                            <button>Visit FAQs</button>
                         </div>
                     </div>
                 </div>
